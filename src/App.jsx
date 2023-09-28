40d0d122bb6917141972d7c2a361fbd3f16ab5b6
DobermannGroup <jake.dev@dobermann.group>
2023-09-28 13:20:55 +0100

    Latest Deploy / Demo Ready Full / Links 2

--- src/App.jsx
+++ src/App.jsx
@@ -56,10 +56,10 @@ function App() {
           <div className="podcastVideo"><iframe width="100%" src="https://www.youtube.com/embed/mGihWTWcdok?si=DPWYT7IaKoTUEr3b" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
           <div className="actionBar podcasts">
             <a href="https://youtu.be/mGihWTWcdok?si=H7kcFg9C_B2V4_W-" target="_blank"><button><i className="fa-brands fa-youtube"></i> Watch on YouTube</button></a>
-            <a href="https://podcasts.apple.com/gb/podcast/life-after-sesh/id1697429061" target="_blank"></a><button><i className="fa-brands fa-apple"></i> Apple Podcasts</button></a>
-            <a href="https://open.spotify.com/show/1bwQAcU92WwJTgT2oiNCNv?si=5ecb82478b0c4623" target="_blank"></a><button><i className="fa-brands fa-spotify"></i> Spotify</button></a>
-            <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5idXp6c3Byb3V0LmNvbS8yMjE4MTI2LnJzcw?sa=X&ved=2ahUKEwjgk5y7zrCAAxW4QKQEHeKiAXcQ9sEGegQIARAC" target="_blank"></a><button><i className="fa-brands fa-google"></i> Google Podcasts</button></a>
-            <a href="https://music.amazon.co.uk/podcasts/9c3cd345-1616-452a-9061-37987e35ebb9/life-after-sesh" target="_blank"></a><button><i class="fa-brands fa-amazon"></i> Google Podcasts</button></a>
+            <a href="https://podcasts.apple.com/gb/podcast/life-after-sesh/id1697429061" target="_blank"><button><i className="fa-brands fa-apple"></i> Apple Podcasts</button></a>
+            <a href="https://open.spotify.com/show/1bwQAcU92WwJTgT2oiNCNv?si=5ecb82478b0c4623" target="_blank"><button><i className="fa-brands fa-spotify"></i> Spotify</button></a>
+            <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5idXp6c3Byb3V0LmNvbS8yMjE4MTI2LnJzcw?sa=X&ved=2ahUKEwjgk5y7zrCAAxW4QKQEHeKiAXcQ9sEGegQIARAC" target="_blank"><button><i className="fa-brands fa-google"></i> Google Podcasts</button></a>
+            <a href="https://music.amazon.co.uk/podcasts/9c3cd345-1616-452a-9061-37987e35ebb9/life-after-sesh" target="_blank"><button><i class="fa-brands fa-amazon"></i> Google Podcasts</button></a>
           </div>
         </div>
 
